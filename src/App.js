b0d1c5c0a5c5788f8d012b2a5a6d7dafd213eb1b
AmauryGranvillain <amaurygranvillain17@gmail.com>
2020-02-25 10:08:02 +0100

    add getFeedbackForCard method + add method flip card + count flip card

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react'
 import shuffle from 'lodash.shuffle'
 
-import './App.css';
+import './App.css'
 
-import Card from './Card';
-import GuessCount from './GuessCount';
-import HallOfFame, { FAKE_HOF } from './HallOfFame';
+import Card from './Card'
+import GuessCount from './GuessCount'
+import HallOfFame, { FAKE_HOF } from './HallOfFame'
 
-const SIDE = 6;
-const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿';
+const SIDE = 6
+const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
 
 class App extends Component {
-  cards = this.generateCards();
-
+  state = {
+    cards: this.generateCards(),
+    currentPair: [],
+    guesses: 0,
+    matchedCardIndices: [],
+  }
+ 
   generateCards() {
     const result = []
     const size = SIDE * SIDE
@@ -24,19 +29,47 @@ class App extends Component {
     return shuffle(result)
   }
 
-  handleCardClick(card) {
-    console.log(card, 'clicked')
+  handleCardClick = index => {
+    const{ currentPair } = this.state
+
+    if(currentPair.length === 2){
+      return
+    }
+
+    if(currentPair.length === 0){
+      this.setState({ currentPair: [index] })
+      return
+    }
+
+    this.handleNewPairClosedBy(index)
+  }
+
+  getFeedbackForCard(index) {
+    const { currentPair, matchedCardIndices} = this.state
+    const indexMatched = matchedCardIndices.includes(index)
+
+    if (currentPair.length < 2){
+      return indexMatched || index === currentPair[0] ? 'visible' : 'hidden'
+    }
+
+    if(currentPair.includes(index)) {
+      return indexMatched ? 'justMatched' : 'justMismatched'
+    }
+    
+    return indexMatched ? 'visible' : 'hidden'
   }
 
   render() {
-    const won = new Date().getSeconds() % 2 === 0;
+    const { cards, guesses, matchedCardIndices} = this.state
+    const won = matchedCardIndices.length === cards.length;
     return (
       <div className="memory">
-        <GuessCount guesses={0} />
-        {this.cards.map((card, index) => (
+        <GuessCount guesses={guesses} />
+        {cards.map((card, index) => (
           <Card
             card={card}
-            feedback="visible"
+            feedback={this.getFeedbackForCard(index)}
+            index={index}
             key={index}
             onClick={this.handleCardClick}
           />
@@ -47,4 +80,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App
--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-import './Card.css';
+import './Card.css'
 
-const HIDDEN_SYMBOL = '❓';
+const HIDDEN_SYMBOL = '❓'
 
-const Card = ({ card, feedback, onClick}) => (
-    <div className={`card ${feedback}`} onClick={() => onClick(card)}>
+const Card = ({ card, feedback, index, onClick}) => (
+    <div className={`card ${feedback}`} onClick={() => onClick(index)}>
         <span className="symbol">
             {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
         </span>
@@ -21,7 +21,8 @@ Card.propTypes = {
       'justMismatched',
       'visible',
     ]).isRequired,
+    index: PropTypes.number.isRequired,
     onClick: PropTypes.func.isRequired,
 }
 
-export default Card;
+export default Card
